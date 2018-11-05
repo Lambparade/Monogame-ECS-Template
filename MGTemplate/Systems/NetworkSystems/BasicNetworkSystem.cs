e34f07cb7ef03c8d9cb846937fd90cf726021079
Connor Robinson <connorrobinsontech@gmail.com>
2018-11-05 12:38:05 -0500

    Fixed formatting issues

--- MGTemplate/Systems/NetworkSystems/BasicNetworkSystem.cs
+++ MGTemplate/Systems/NetworkSystems/BasicNetworkSystem.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+using MGTemplate.Components;
+
+using MGTemplate.Systems.Content_System;
+
+using MGTemplate.Managers.Graphics_Managers;
+
+using MGTemplate.Entities;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 namespace MGTemplate.Systems.NetworkSystems
 {
     public class BasicNetworkSystem
     {
-        
+        string ServerIP = string.Empty;
+
+        public BasicNetworkSystem()
+        {
+
+        }
     }
 }
